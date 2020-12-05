3cdda45987f53f711ab0f9e0a0852d46c550043d
Emeric SPIROUX <e.spiroux@groupeonepoint.com>
2020-12-05 16:24:33 +0100

    [back] :recycle: Refacto Dice change and init to TimeController

--- back/src/Controllers/TimerController.ts
+++ back/src/Controllers/TimerController.ts
@@ -5,15 +5,16 @@ import AppController, { IAppController } from './Types/AppController.abstract';
 import AppRouteDescriptor from './Types/AppRouteDescriptor.type';
 import FormErrorMiddleware from './validations/Errors/FormErrorMiddleware';
 import ControllerError from './Errors/ControllerError';
-import PackageJSON from '../../package.json';
-import { getRootPath } from '../libs/system/getRootPath';
-import { DateRangeValidations } from './validations/Atoms/DateValidations';
 import { DiceFaceOrDateValidations } from './validations/Organismes/DiceFaceOrDateValidations';
 import DiceFaceTime from '../Models/DiceFaceTime';
 import ModelError from '../Models/Errors/ModelError';
+import DiceFace from '../Models/DiceFace';
+import DiceObject from '../libs/DiceEngine/DiceObject/DiceObject';
+import SocketServeur from '../Serveur/Sockets/SockerServeur';
 
 export default class DiceController extends AppController implements IAppController {
 	baseRoute: AppRouteDescriptor;
+	private previous = -1;
 
 	constructor() {
 		super();
@@ -27,6 +28,61 @@ export default class DiceController extends AppController implements IAppControl
 		this.router.get('/', DiceFaceOrDateValidations, FormErrorMiddleware, this.getRange.bind(this));
 		this.router.get('/current', this.getCurrent.bind(this));
 		return this.router;
+	}
+
+	async onChangeDiceFace(dice: DiceObject) {
+		logguer.i('Detect dice motion face :', dice.face);
+		if (dice.face !== this.previous) {
+			logguer.i('Not same as this.previous :', this.previous);
+			this.previous = dice.face;
+			try {
+				const diceFaceTimeStop = await DiceFaceTime.stop();
+				logguer.d('Stoping diceFaceTime :', diceFaceTimeStop);
+				logguer.d('Stoping diceFaceTime duration :', diceFaceTimeStop.duration);
+				if (
+					process.env.TIMETOUT_DURATION_BEFORE_SAVE &&
+					diceFaceTimeStop.duration < Number(process.env.TIMETOUT_DURATION_BEFORE_SAVE)
+				) {
+					logguer.d('Deleting diceFaceTime because too short :', diceFaceTimeStop.id);
+					await DiceFaceTime.deleting(diceFaceTimeStop.id);
+				}
+			} catch (err) {
+				logguer.d('Error on stopping diceFaceTime :', err);
+			}
+			if (dice.face !== -1) {
+				const diceFace = await DiceFace.getFace(dice.face);
+				const diceFaceTimeStart = await DiceFaceTime.start(diceFace);
+				SocketServeur.shared.io.emit('dice.start', diceFaceTimeStart);
+				logguer.d('Starting diceFaceTime :', diceFaceTimeStart);
+			} else {
+				SocketServeur.shared.io.emit('stop');
+			}
+		}
+		if (dice.isOff && this.previous !== -1) {
+			this.previous = -1;
+			await DiceFaceTime.stop();
+			SocketServeur.shared.io.emit('dice.stop');
+		}
+	}
+
+	async InitFaceDefault() {
+		logguer.d('DiceFaces init...');
+		if (await DiceFace.getFace(1)) return logguer.d('DiceFaces already inited. Skipped.');
+		const faceIds = [1, 2, 3, 4, 5, 6, 7, 8];
+		const faceTitle = [
+			'Code',
+			'Code Review',
+			'Réunion',
+			'Read Documentation',
+			'Help others',
+			'Pause',
+			'debug',
+			'Write documentation',
+		];
+		for (const [index, faceId] of faceIds.entries()) {
+			const dice = await DiceFace.define(true, faceId, faceTitle[index] || `Face ${faceId}`, 'blue');
+			logguer.d('DiceFaces define :', dice);
+		}
 	}
 
 	private async getRange(req: Request, res: Response, next: NextFunction): Promise<void> {
--- back/src/app.ts
+++ back/src/app.ts
@@ -8,9 +8,6 @@ import PackageJson from '../package.json';
 import logger from 'basic-log';
 
 import DiceEngine from './libs/DiceEngine/Engine/DiceEngine';
-import DiceObject from './libs/DiceEngine/DiceObject/DiceObject';
-import DiceFace from './Models/DiceFace';
-import DiceFaceTime from './Models/DiceFaceTime';
 import TimerController from './Controllers/TimerController';
 
 // Getting env var
@@ -29,61 +26,26 @@ dotenv.config({
 	})(),
 });
 
-// Creating server
-let serveur = new Serveur({
-	mongo: {
-		uri: process.env.MONGO_URI,
-	},
-});
-
-// Add controllers
-serveur.router.addController(new TimerController());
-serveur.router.addController(new SocketController());
-
-// Start listenning
-serveur.start(Number(process.env.PORT));
-
-// Launching dice detection
-
-async function InitFaceDefault() {
-	const faceIds = [1, 2, 3, 4, 5, 6, 7, 8];
-	const faceTitle = ['Code', 'Code Review', 'Réunion', 'Documentation', 'Relecture', 'Pause'];
-	for (const [index, faceId] of faceIds.entries()) {
-		DiceFace.define(true, faceId, faceTitle[index] || `Face ${faceId}`, 'blue');
-	}
-}
-
 (async () => {
-	await InitFaceDefault();
-	let previous = -1;
-	DiceEngine.shared.start(async (dice: DiceObject) => {
-		logger.i('Detect dice motion face :', dice.face);
-		if (dice.face !== previous) {
-			logger.i('Not same as previous :', previous);
-			previous = dice.face;
-			try {
-				const diceFaceTimeStop = await DiceFaceTime.stop();
-				logger.d('Stoping diceFaceTime :', diceFaceTimeStop);
-				logger.d('Stoping diceFaceTime duration :', diceFaceTimeStop.duration);
-				if (
-					process.env.TIMETOUT_DURATION_BEFORE_SAVE &&
-					diceFaceTimeStop.duration < Number(process.env.TIMETOUT_DURATION_BEFORE_SAVE)
-				) {
-					logger.d('Deleting diceFaceTime because too short :', diceFaceTimeStop.id);
-					await DiceFaceTime.deleting(diceFaceTimeStop.id);
-				}
-			} catch (err) {
-				logger.d('Error on stopping diceFaceTime :', err);
-			}
-			if (dice.face !== -1) {
-				const diceFace = await DiceFace.getFace(dice.face);
-				const diceFaceTimeStart = await DiceFaceTime.start(diceFace);
-				logger.d('Starting diceFaceTime :', diceFaceTimeStart);
-			}
-		}
-		if (dice.isOff && previous !== -1) {
-			previous = -1;
-			await DiceFaceTime.stop();
-		}
+	// Creating server
+	let serveur = new Serveur({
+		mongo: {
+			uri: process.env.MONGO_URI,
+		},
 	});
+
+	// Add controllers
+	const timerController = new TimerController();
+
+	serveur.router.addController(new TimerController());
+	serveur.router.addController(new SocketController());
+
+	// Start listenning
+	serveur.start(Number(process.env.PORT));
+
+	// Init dices faces
+	await timerController.InitFaceDefault();
+
+	// Launching dice detection
+	DiceEngine.shared.start(timerController.onChangeDiceFace);
 })();
